996179f90b5b6fb9432eedda2547c74d990a628f
Glenn Rempe <glenn@truestamp.com>
2022-05-18 02:20:30 -0400

    Update deno example imports

--- examples/deno/index.ts
+++ examples/deno/index.ts
@@ -6,15 +6,15 @@
 
 // Load from local lib in development
 // import { canonify } from "../../lib/index.mjs"
-// import { canonify } from "../../mod.ts"
+import { canonify } from "../../mod.ts"
 
 // Or, Load the module from deno.land/x/canonify
 // @ts-ignore-next-line
-// import canonify from "https://deno.land/x/canonify@v1.1.0/mod.ts";
+// import { canonify } from "https://deno.land/x/canonify@v1.1.1/mod.ts";
 
 // Or, load latest ES Module from SkyPack. You should really use a pinned URL!
 // See : https://docs.skypack.dev/skypack-cdn/code/optimize-for-production
-import canonify from "https://cdn.skypack.dev/@truestamp/canonify?dts";
+// import { canonify } from "https://cdn.skypack.dev/@truestamp/canonify@1.1.1?dts";
 
 const example = {
   big: BigInt(42).toString(),
